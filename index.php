9c594f574780f1e8f0463c928ee676b36f875dce
WangYeCoder <w258765@gmail.com>
2020-03-01 12:54:51 +0800

    123

--- index.php
+++ index.php
@@ -17,10 +17,12 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 }
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);
 
 // 定义应用目录
 define('APP_PATH', './Application/');
+
+define('CONF_PATH',"./Application/Home/Conf/config.php");
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
